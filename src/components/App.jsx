e264ad0f8e822a22654232e9d909b604e103f25e
OlegMishko <oleg.mishko@gmail.com>
2022-10-06 00:19:23 +0300

    base
    добавлено основа,

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+import fetchPics from './service/ApiService';
+
+import { MainPage } from 'common/MainPage.styled';
+
+export default class App extends Component {
+  state = {
+    searchQuery: '',
+    page: 1,
+  };
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    console.log('componentDidUpdate');
+  }
+
+  render() {
+    const {} = this.state;
+
+    return (
+      <MainPage>
+        <SearhBar />
+        <ImageGallery />
+        <Button />
+        <Modal />
+      </MainPage>
+    );
+  }
+}
---
+++ src/components/service/ApiService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '29186730-3f44e5d7f92932d3e32146242';
+
+async function fetchPics(page, searchQuery) {
+  const responce = await axios.get(
+    `?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return responce.data;
+}
+
+export default fetchPics;
